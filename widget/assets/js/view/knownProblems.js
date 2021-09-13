6a700c405e395a2c091d2aa9e883b930e2113aa3
Jay C <jay.gch93@gmail.com>
2021-09-13 21:41:29 +0200

    change pre commit and update package, again

--- widget/assets/js/view/knownProblems.js
+++
@@ -1,9 +0,0 @@
-var BugAdaptors;
-(function (BugAdaptors) {
-  BugAdaptors[(BugAdaptors['Jira'] = 0)] = 'Jira';
-})(BugAdaptors || (BugAdaptors = {}));
-export class KnownProblemUi {
-  constructor(bugAdaptor) {
-    this.bugAdaptor = bugAdaptor;
-  }
-}
